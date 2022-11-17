789d90e1cb564649d1dc5e429a1b5770d781a89e
Rashmi Nema <rashmi631995@gmail.com>
2022-11-17 12:44:07 +0530

    Added Code

---
+++ Terraform-1/ec2.tf
@@ -0,0 +1,87 @@
+# Creating 1st EC2 instance in Public Subnet
+resource "aws_instance" "demoinstance" {
+  ami                         = var.ami
+  instance_type               = "t2.micro"
+  count                       = 1
+  key_name                    = "tests"
+  vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
+  subnet_id                   = "${aws_subnet.demoinstance.id}"
+  associate_public_ip_address = true
+  user_data                   = "${file("data.sh")}"
+tags = {
+    Name = "My Public Instance"
+  }
+}
+# Creating 2nd EC2 instance in Public Subnet
+resource "aws_instance" "demoinstance1" {
+  ami                         = var.ami
+  instance_type               = "t2.micro"
+  count                       = 1
+  key_name                    = "tests"
+  vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
+  subnet_id                   = "${aws_subnet.demoinstance.id}"
+  associate_public_ip_address = true
+tags = {
+    Name = "My Public Instance 2"
+  }
+}
+
+# Creating Security Group 
+resource "aws_security_group" "demosg" {
+  vpc_id = "${aws_vpc.demovpc.id}"
+# Inbound Rules
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+# HTTPS access from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+# SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+# Outbound Rules
+  # Internet access to anywhere
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+tags = {
+    Name = "Web SG"
+  }
+}
+
+# Create Database Security Group
+resource "aws_security_group" "database-sg" {
+  name        = "Database SG"
+  description = "Allow inbound traffic from application layer"
+  vpc_id      = aws_vpc.demovpc.id
+ingress {
+    description     = "Allow traffic from application layer"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.demosg.id]
+  }
+egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+tags = {
+    Name = "Database SG"
+  }
